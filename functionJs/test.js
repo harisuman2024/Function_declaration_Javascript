7832240f958b375715f48c3ba6e7080c79c08a17
HARI SUMAN <95356493+harisuman2024@users.noreply.github.com>
2023-08-17 17:20:38 +0000

    function declaration in javascript

---
+++ functionJs/test.js
@@ -0,0 +1,7 @@
+function addNumbers(a, b) {
+    return a + b;
+  }
+  
+  const sum = addNumbers(5, 7); // Calling the function and storing the result in the 'sum' variable
+  console.log(sum); // Output: 12
+  
